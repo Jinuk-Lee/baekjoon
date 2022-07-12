bce7aa5605dde8f347f2222213fec2acc952e33b
Jinuk Lee <20181680@edu.hanbat.ac.kr>
2022-07-12 15:40:34 +0900

    split을 이용해 문자열로 입력받은 숫자들로 만들 수 있는 가장 큰수 만들기

--- src/MaxNumber/Solution.java
+++ src/MaxNumber/Solution.java
@@ -1,13 +1,9 @@
 package MaxNumber;
-
 import java.util.*;
 
-
-
 class Solution {
-    static int listsize;
-    //static int[] number;
-
+//    static int listsize;
+//    static int[] number;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,14 +11,12 @@ class Solution {
         String num = sc.next();
         String[] list = num.split(",");
 
-        listsize = list.length;  //입력받은 숫자 개수
+        //listsize = list.length;  입력받은 숫자 개수
 //        number = new int[listsize]; //개수만큼 배열 선언
-//
 //        for (int i = 0; i < listsize; i ++) {
 //           number[i] = Integer.parseInt(list[i]);//string -> int
 //
 //        }
-
         Arrays.sort(list, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -30,16 +24,11 @@ class Solution {
             }
         });
 
-        for(int i =0;i<listsize;i++){
+        for(int i =0;i<list.length;i++){
            // System.out.println(list[i]);
             answer += list[i];
         }
+
         System.out.println(answer);
-
-
-
     }
-
-
-
 }
