6427ead3535087dbd45f9589d925d502fa538edd
jinuklee <20181680@edu.hanbat.ac.kr>
2022-07-18 22:36:45 +0900

    문제 설명과 접근 방식 기록

--- src/shortpath/Solution.java
+++ src/shortpath/Solution.java
@@ -2,22 +2,26 @@ package shortpath;
 
 import java.util.*;
 
-class Edge<W, V>  {
+class Edge<W, V> {
     private W weight;
     private V v;
+
     public Edge(V i) {
     }
-    public void setEdge(V v,W weight) {
+
+    public void setEdge(V v, W weight) {
         this.v = v;
         this.weight = weight;
     }
 }
+
 public class Solution {
     private static int edgecount;  //간선의 개수
     private static int nodecount;   //노드의 개수
     private static int startnode; //시작 정점
     private static int weight;  //가중치
     private static ArrayList<Edge<Integer, Integer>> graph = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -36,20 +40,22 @@ public class Solution {
             int node2 = sc.nextInt(); //종료 노드
             weight = sc.nextInt();  //해당 노드 가중치 설정
 
-            createEdge(node1,node2,weight);
+            createEdge(node1, node2, weight);
 
         }
         for (int i = 0; i < graph.size(); i++) {
             System.out.println();
         }
     }
-    public static void createNode ( int N){
+
+    private static void createNode(int N) {
         for (int i = 0; i < N; i++) {
             graph.add(new Edge<>(i));    //노드의 개수만큼 노드 생성
         }
     }
-    public static void createEdge(int node1,int node2,int weight) {
-        graph.get(node1).setEdge(node2,weight); //간선의 정보를 통해 간선 생성
+
+    private static void createEdge(int node1, int node2, int weight) {
+        graph.get(node1).setEdge(node2, weight); //간선의 정보를 통해 간선 생성
         //graph.get(node2).setEdge(node1,weight); 양방향일때 반대 간선
 
     }
