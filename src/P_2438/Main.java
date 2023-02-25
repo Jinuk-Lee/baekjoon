dbe7ad52ed0015504fe69a4521b0be47f02c1e71
leejinuk <lee991227@gmail.com>
2023-02-26 01:33:49 +0900

    별 찍기 -1

--- src/P_2438/Main.java
+++ src/P_2438/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a =Integer.parseInt(st.nextToken());
-        for(int i=1; i<=a;i++){
-            for(int j =0; j<i;j++){
+        int a = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
