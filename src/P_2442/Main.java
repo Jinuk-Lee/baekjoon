e11a56a3170c562cf5459a4535f9168462e3bcf5
leejinuk <lee991227@gmail.com>
2023-02-26 02:01:42 +0900

    별 찍기 -5

---
+++ src/P_2442/Main.java
@@ -0,0 +1,23 @@
+package P_2442;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < a-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < ((2 * i) - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
