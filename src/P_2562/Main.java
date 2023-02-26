e583d2542b458b86561f53d343d7bb168af37e53
leejinuk <lee991227@gmail.com>
2023-02-26 13:43:10 +0900

    최댓값

--- src/P_2562/Main.java
+++ src/P_2562/Main.java
@@ -6,13 +6,10 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int[] num = new int[9];
 
         for (int i = 0; i < num.length; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            num[i] = Integer.parseInt(st.nextToken());
+            num[i] = Integer.parseInt(br.readLine());
         }
         int max = 0;
         int index = 0;
