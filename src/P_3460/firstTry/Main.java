3e10998f656d70479ca07eb073d24f71da1eadb1
leejinuk <lee991227@gmail.com>
2023-03-01 21:45:20 +0900

    이진수 정답 코드

--- src/P_3460/firstTry/Main.java
+++ src/P_3460/firstTry/Main.java
@@ -3,7 +3,6 @@ package P_3460.firstTry;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,19 +11,18 @@ public class Main {
 
         int Case = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] N = new int[Case];
 
+        for (int i = 0; i < Case; i++) {
+            N[i] = Integer.parseInt(br.readLine());
+        }
         for (int j = 0; j < Case; j++) {
-            int n = 0;
             int count = 0;
-            if (st.hasMoreTokens()) {
-                n = Integer.parseInt(st.nextToken());
-            }
-            while (n > 0) {
-                if (n % 2 == 1)
+            while (N[j] > 0) {
+                if (N[j] % 2 == 1)
                     System.out.print(count + " ");
                 count++;
-                n = n / 2;
+                N[j] = N[j] / 2;
             }
             System.out.println();
 
